2f0870475c858fc2f46fcde8ad8495c4ee566a78
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-22 22:54:56 -0300

    69. Usando parciales con HBS

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
 
+const hbs = require('hbs');
+
 app.use( express.static(__dirname + '/public' ));
 
 // Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
 
 app.get('/', (req, res) => {
     res.render('home', {
         nombre: 'Thiago Bianeck',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
         anio: new Date().getFullYear()
     });
 });
