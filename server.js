172cc3dc3d5bd7a59e6a54975ed8601722e7e45a
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-23 00:16:56 -0300

    71. Heroku - Subiendo nuestra aplicación a producción

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
 
 app.use( express.static(__dirname + '/public' ));
 
@@ -10,20 +13,19 @@ hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
 
+
+
 app.get('/', (req, res) => {
     res.render('home', {
-        nombre: 'Thiago Bianeck',
-        anio: new Date().getFullYear()
+        nombre: 'Thiago Bianeck'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', {
-        anio: new Date().getFullYear()
-    });
+    res.render('about');
 });
 
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 });
